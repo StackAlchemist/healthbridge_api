398bd4972a17fa924fa0c997824d00d7a8f0c434
StackAlchemist <jolamideprince@gmail.com>
2026-02-16 11:53:06 +0100

    req methods for appt

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -247,7 +247,7 @@ export const getApppointsByDoctorId = async (req, res) => {
 
 export const getAppointments = async (req, res) => {
   try {
-    const { doctorId } = req.body || req.params || req.query; 
+    const doctorId = req.body.doctorId || req.params.doctorId || req.query.doctorId; 
     
     // Find the doctor by ID
     const doctor = await Doctor.findById(doctorId);
